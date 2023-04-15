31220d4899d4a231758023dff1058e8e156074c4
Angel Lopez <angel@Angels-Laptop.local>
2023-04-15 17:46:20 -0400

    fixed error message on startup and added codeium

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -21,7 +21,11 @@ vim.opt.incsearch = true
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+require("notify").setup({
+  background_colour = "#000000",
+})
+vim.api.nvim_command("au ColorScheme * hi Normal ctermbg=None")
+--
 --vim.api.nvim_exec([[:lua require("persistence").load()]], false)
 
 --autocmd:execute("s")
