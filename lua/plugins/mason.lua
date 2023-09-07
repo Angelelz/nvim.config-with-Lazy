75a4a4a81693258016490210c290f0c5dd1d02df
Angelelz <angellopez20@gmail.com>
2023-09-06 22:39:50 -0400

    This is not my real config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,8 +1,8 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = function(_, opts)
-      -- table.insert(opts.ensure_installed, "prettierd")
-    end,
+    -- opts = function(_, opts)
+    --   table.insert(opts.ensure_installed, "prettierd")
+    -- end,
   },
 }
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,6 +4,7 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.prettierd)
+      -- table.insert(opts.sources, nls.builtins.formatting.dprint.with(require('plugins.dprint')))
     end,
   },
 }
