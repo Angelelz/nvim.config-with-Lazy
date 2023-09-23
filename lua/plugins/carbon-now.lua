20f4ac9bd603cfc00d9ace833c2d5b13ed8ad7fe
Angelelz <angellopez20@gmail.com>
2023-09-23 16:58:15 -0400

    Added carbon

---
+++ lua/plugins/carbon-now.lua
@@ -0,0 +1,8 @@
+return {
+  "kristijanhusak/vim-carbon-now-sh",
+  lazy = true,
+  -- cmd = "CarbonNow",
+  keys = {
+    { "<leader>ct", ":CarbonNowSh<cn>", mode = "v", desc = "Carbon Now" },
+  },
+}
