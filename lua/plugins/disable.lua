73b1e5df61d016bd582225baacf8e112e4102ee9
Angelelz <angellopez20@gmail.com>
2024-01-27 10:36:26 -0500

    disable mini.pairs

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -2,4 +2,5 @@ return {
   --   { "folke/noice.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "echasnovske/mini.pairs", enabled = false },
 }
