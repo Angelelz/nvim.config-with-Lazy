16c2317e81955465a82748cdc3ddf60fb8e8f6e5
Angelelz <angellopez20@gmail.com>
2024-02-16 18:34:11 -0500

    remove import cost

--- lua/plugins/import-cost.lua
+++ lua/plugins/import-cost.lua
@@ -1,11 +1,11 @@
-function OS()
-  return package.config:sub(1, 1) == "\\" and "win" or "unix"
-end
+-- function OS()
+--   return package.config:sub(1, 1) == "\\" and "win" or "unix"
+-- end
 return {
-  "barrett-ruth/import-cost.nvim",
-  -- build = "sh install.sh yarn",
-  build = OS() == "win" and "pwsh install.ps1 npm" or "sh install.sh npm",
-  -- if on windows
-  -- build = 'pwsh install.ps1 yarn',
-  config = true,
+  -- "barrett-ruth/import-cost.nvim",
+  -- -- build = "sh install.sh yarn",
+  -- build = OS() == "win" and "pwsh install.ps1 npm" or "sh install.sh npm",
+  -- -- if on windows
+  -- -- build = 'pwsh install.ps1 yarn',
+  -- config = true,
 }
